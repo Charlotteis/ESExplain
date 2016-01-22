ecafbcf1b59acea2643a242a5498bee1e2ebe118
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-01-22 16:45:53 +0000

    feat: implement sidebar design

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-var heart = document.getElementById('js-emoji-heart')
-var computer = document.getElementById('js-emoji-computer')
+var heart = document.getElementById('js-emoji-heart');
+var computer = document.getElementById('js-emoji-computer');
+var articlesToggle = document.getElementById('js-articles-toggle');
+var sidebarNav = document.getElementById('js-articles-nav');
 
-heart.innerHTML = twemoji.parse('\uD83D\uDC96')
-computer.innerHTML = twemoji.parse('\uD83D\uDCBB')
+heart.innerHTML = twemoji.parse('\uD83D\uDC96');
+computer.innerHTML = twemoji.parse('\uD83D\uDCBB');
+
+articlesToggle.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  if (sidebarNav.style.display === 'none' || sidebarNav.style.display === '') {
+    sidebarNav.style.display = 'block';
+    articlesToggle.textContent = 'Hide Articles List';
+  } else {
+    sidebarNav.style.display = 'none';
+    articlesToggle.textContent = 'Show Articles List';
+  }
+});
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 600) {
+    sidebarNav.style.display = 'block';
+  } else {
+    sidebarNav.style.display = 'none';
+    articlesToggle.textContent = 'Show Articles List';
+  }
+});
