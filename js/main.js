1280b89723b7f15ba251ca3f6db371e676ffa5e4
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-01-22 14:34:46 +0000

    feat: implement footer design

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var heart = document.getElementById('js-emoji-heart')
+var computer = document.getElementById('js-emoji-computer')
+
+heart.innerHTML = twemoji.parse('\uD83D\uDC96')
+computer.innerHTML = twemoji.parse('\uD83D\uDCBB')
